b066401e272ad73720dee604f354d51177b13e90
alievanigar <82894919+alievanigar@users.noreply.github.com>
2021-10-26 22:52:17 +0400

    Delete moneymanager directory

--- moneymanager/src/main/java/com/example/moneymanager/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.moneymanager.repository;
-
-import com.example.moneymanager.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Integer> {
-}
